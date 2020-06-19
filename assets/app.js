1f94fb5348d1ab3a2994266c4cb2d09cad3e2a60
CircuitSyn <circuitsyn@gmail.com>
2020-06-19 00:01:49 -0700

    working snapshot button - annimation needs fixing for class add and removal, css added locally

--- assets/app.js
+++ assets/app.js
@@ -2,10 +2,45 @@ $(document).ready(function() {
 
     $(function() { $("#snapBtn").click(function (event) { 
         $.getJSON('/PhotoSnap', { },
-    function(data) { }); return false; }); });
+    function(data) { }); return false; }); 
+    // trigger snap button animation
+    animateCSS('#snapBtn', 'animate__rubberBand');
+    });
 
 
+    // const animateCSS = (element, animation, prefix = 'animate__') =>
+    // // We create a Promise and return it
+    // new Promise((resolve, reject) => {
+    //     const animationName = `${prefix}${animation}`;
+    //     const node = document.querySelector(element);
 
+    //     node.classList.add(`${prefix}animated`, animationName);
+
+    //     // When the animation ends, we clean the classes and resolve the Promise
+    //     function handleAnimationEnd() {
+    //     node.classList.remove(`${prefix}animated`, animationName);
+    //     node.removeEventListener('animationend', handleAnimationEnd);
+
+    //     resolve('Animation ended');
+    //     }
+
+    //     node.addEventListener('animationend', handleAnimationEnd);
+    // });
+
+    // annimate.css function
+    animateCSS = (element, animationName, callback) => {
+        const node = document.querySelector(element)
+        node.classList.add('animated', animationName)
+
+    function handleAnimationEnd() {
+    node.classList.remove('animated', animationName)
+    node.removeEventListener('animationend', handleAnimationEnd)
+
+    if (typeof callback === 'function') callback()
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd)
+        }
 
 
 
--- picamapp.py
+++ picamapp.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, jsonify, request
 import picamera
 import cv2
 import socket
 import io
+import os
+import time
 
 app = Flask(__name__)
 vc = cv2.VideoCapture(0)
@@ -16,8 +18,10 @@ def gen():
     """Video streaming generator function."""
     while True:
         rval, frame = vc.read()
-        #flip code to turn camera view upside down
         frame = cv2.flip(frame, -1)
+        # cv2.imwrite('t.jpg', frame)
+        # yield (b'--frame\r\n'
+        #        b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
         byteArray = cv2.imencode('.jpg', frame)[1].tobytes()
         yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + byteArray + b'\r\n')
@@ -27,6 +31,24 @@ def video_feed():
     """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Photo Snap Function
+@app.route('/PhotoSnap')
+def PhotoSnap():
+    print('In PhotoSnap')
+    # command = "raspistill -o \shared\img.jpg"
+    # os.system(command)
+
+    #set a variable with the current time to use as image file name
+    curTime = (time.strftime("%I%M%S"))
+    print(curTime)
+    #take a photo, give it a name, and resize it to fit into Parse
+    # cam.capture(curTime, resize=(320,240))
+    rval, frame = vc.read()
+    frame = cv2.flip(frame, -1)
+    # code to write frame to jpeg with timestamp as filename
+    cv2.imwrite('/home/pi/shared/3DPrinterCam/photos/'+ curTime  +'.jpg', frame)
+    return "Nothing"
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
