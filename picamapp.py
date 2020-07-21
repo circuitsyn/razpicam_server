4c19a8be63ccbbb0870b389676b437ea7cf9edc7
CircuitSyn <circuitsyn@gmail.com>
2020-07-21 13:13:13 -0700

    adding missing files

---
+++ picamapp.py
@@ -0,0 +1,220 @@
+from flask import Flask, render_template, Response, jsonify, request
+from flask_socketio import SocketIO, emit
+import picamera
+import SensorCode
+from bleson import get_provider, Observer, UUID16
+import json
+import cv2
+import socket
+import io
+import os
+import glob
+import time
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'include_help!'
+socketio = SocketIO(app, async_mode='threading')
+# os.urandom(12)
+
+vc = cv2.VideoCapture(0)
+vc.set(3, 1280)
+vc.set(4, 720)
+
+@app.route('/')
+def index():
+    """Video streaming"""
+    return render_template('index.html')
+
+def gen():
+    """Video streaming generator function."""
+    while True:
+        rval, frame = vc.read()
+        # frame = cv2.flip(frame, -1)
+        # cv2.imwrite('t.jpg', frame)
+        # yield (b'--frame\r\n'
+        #        b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+        byteArray = cv2.imencode('.jpg', frame)[1].tobytes()
+        yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + byteArray + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+# Photo Snap Function
+@app.route('/PhotoSnap', methods=['GET', 'POST'])
+def PhotoSnap():
+    print("snap snap")
+    #set a variable with the current time to use as image file name
+    curTime = (time.strftime("%m%j%y"+"_"+"%H%M_%S"))
+    #take a photo, give it a name, and resize it to fit into Parse
+    rval, frame = vc.read()
+    # frame = cv2.flip(frame, -1)
+    width = vc.get(3)
+    height = vc.get(4)
+    print("width: ", width, "height: ", height)
+
+    snapPath = '/home/pi/shared/3DPrinterCam/static/imgs/captures/'
+
+    # check if capture path exists, create if not
+    if not os.path.exists(snapPath):
+        os.mkdir(snapPath)
+    
+    else:
+        if rval:
+            # code to write frame to jpeg with timestamp as filename
+            isWritten = cv2.imwrite(snapPath + curTime +'.jpg', frame)
+            if isWritten:
+                print('image successfully written to ' + '/home/pi/shared/3DPrinterCam/static/imgs/captures/'+ curTime +'.jpg' )
+            else:
+                print('img write failed')
+        else:
+            print('rval - false - check photo snap else section of code', rval)
+
+    return "Nothing"
+
+# Delete Photo Function
+@app.route('/PhotoDelete', methods=['GET', 'POST'])
+def PhotoDelete():
+    filenameVal = request.form['filenameVal']
+    print('filename: ', filenameVal)
+    os.remove(filenameVal)
+    print("confirmed deletion")
+    return "Nothing"
+
+# Gallery Provide & Update
+@app.route('/photoGalleryBuild', methods=['GET', 'POST'])
+def photoGalleryBuild():
+    path = "/home/pi/shared/3DPrinterCam/static/imgs/captures/"
+    imgData = map(os.path.basename, sorted(glob.glob(path + "*jpg"), reverse=True))
+    # print('imgdata: ', list(imgData))
+    
+    return jsonify({"imgArray": list(imgData)})
+
+# -------------------- functions ----------------------------------
+def timelapse_func(numFrames, delay):
+
+    timelapseSnapPath = '/home/pi/shared/3DPrinterCam/static/imgs/timelapse/'
+
+    # check if capture path exists, create if not
+    if not os.path.exists(timelapseSnapPath):
+        os.mkdir(timelapseSnapPath)
+    
+    else:
+
+        for i in range(numFrames):
+            #set a variable with the current time to use as image file name
+            curTime = (time.strftime('timelapse' + "%m%j%y" + "_" + "%H%M_%S"))
+            #take a photo, give it a name, and resize it to fit into Parse
+            rval, frame = vc.read()
+
+            if rval:
+                # code to write frame to jpeg with timestamp as filename
+                isWritten = cv2.imwrite('/home/pi/shared/3DPrinterCam/static/imgs/timelapse/'+ curTime +'.jpg', frame)
+                if isWritten:
+                    print('image successfully written to ' + '/home/pi/shared/3DPrinterCam/static/imgs/timelapse/'+ curTime +'.jpg' )
+                else:
+                    print('img write failed')
+            else:
+                print('rval - false - check photo snap else section of code', rval)
+            print("timelapse snap")
+            # wait 5 seconds
+            time.sleep(delay)
+
+# -------------------- SocketIO -----------------------------------
+@socketio.on('my event', namespace='/razData')
+def handle_json(json):
+
+    print('received json: ' + str(json))
+    shotsTaken = 0
+    daysValue = int(json['daysValue'])
+    hrsValue = int(json['hrsValue'])
+    minsValue = int(json['minsValue'])
+    secsValue = int(json['secsValue'])
+    delayValue = int(json['delayValue'])
+
+    # calculate sum number of seconds for timelapse
+    sumTime = (daysValue * 24 * 60 * 60) + (hrsValue * 60 * 60) + (minsValue * 60) + (secsValue)
+
+    # calculate total frames to be taken
+    totalFrames = int(sumTime/delayValue)
+
+    timelapseSnapPath = '/home/pi/shared/3DPrinterCam/static/imgs/timelapse'
+
+    # check if capture path exists, create if not
+    if not os.path.exists(timelapseSnapPath):
+        os.mkdir(timelapseSnapPath)
+    
+
+    # >> looping timelapse functionality <<
+    for i in range(totalFrames):
+        #set a variable with the current time to use as image file name
+        curTimelapseTime = (time.strftime('timelapse' + "%m%j%y" + "_" + "%H%M_%S"))
+        #take a photo, give it a name, and resize it to fit into Parse
+        rval, frame = vc.read()
+
+        if rval:
+            # code to write frame to jpeg with timestamp as filename
+            isWritten = cv2.imwrite('/home/pi/shared/3DPrinterCam/static/imgs/timelapse/'+ curTimelapseTime +'.jpg', frame)
+            shotsTaken += 1
+            remainingFrames = totalFrames - shotsTaken
+            if isWritten:
+                print('image successfully written to ' + '/home/pi/shared/3DPrinterCam/static/imgs/timelapse/'+ curTimelapseTime +'.jpg' )
+            else:
+                print('img write failed')
+        else:
+            print('rval - false - check photo snap else section of code', rval)
+        print("timelapse snap")
+        
+        # control boolean status of loading gif by tracking remaining frames
+        if(remainingFrames > 0):
+            loadStatus = 'True'
+        else:
+            loadStatus = 'False'
+            print("Finished Timelapse!")
+
+        # build data json object
+        dataUpdate = {
+            'framesRemaining': remainingFrames,
+            'totalFrames': totalFrames,
+            'loadStatus': loadStatus
+        }
+        emit('timelapseUpdate', dataUpdate)
+
+        # wait 5 seconds
+        time.sleep(delayValue)    
+
+# @socketio.on('my broadcast event', namespace='/razData')
+# def test_message(message):
+#     emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/razData')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/razData')
+def test_disconnect():
+    print('Client disconnected')
+
+@socketio.on('sensorUpdateRequest', namespace='/razData')
+def sensor_data(json):
+    print('sensor message:', json)
+    adapter = get_provider().get_adapter()
+
+    observer = Observer(adapter)
+    observer.on_advertising_data = SensorCode.on_advertisement
+    print('observer data?', observer.on_advertising_data)
+    print('variable print before start', SensorCode.sensorData)
+    observer.start()
+    print('variable print after start', SensorCode.sensorData)
+    time.sleep(2)
+    print('variable print after sleep', SensorCode.sensorData)
+    observer.stop()
+    emit('tempHumSensorUpdate', {'data': 'Connected'})
+
+if __name__ == '__main__':
+    # app.run(debug=False, host='0.0.0.0')
+    socketio.run(app, host='0.0.0.0')
+    
---
+++ static/app.js
@@ -0,0 +1,341 @@
+$(document).ready(function() {
+
+    // Global variables
+    let currentPage = 1;
+    let numPerPage = 32;
+    let numOfPages = 0;
+    let pageListArr = [];
+
+
+    // --------------------- Flask WebSocketIO Start -------------------------------
+    var socket = io.connect('http://' + document.domain + ':' + location.port + '/razData');
+    sensorTempHumSwitch
+    // Temp/Humidity Sensor Data
+    $(function() { $("#sensorTempHumSwitch").click(function (e) { 
+        // e.preventDefault();
+        console.log('toggle clicked')
+        if($("#sensorTempHumSwitch").is(':checked')){
+            // if checked ignore duplicate request action
+            console.log('already checked')
+            socket.emit('sensorUpdateRequest', {
+                msg: 'requesting sensor data',
+                sensorStatus: 'False'
+            });
+        }
+        else {
+            // unchecked request sensor data
+            socket.emit('sensorUpdateRequest', {
+                msg: 'requesting sensor data',
+                sensorStatus: 'True'
+            }); 
+        }
+
+    // socket sensor data 
+    socket.on('tempHumSensorUpdate', function(sensorData) {
+        console.log('tempHumSensorUpdate returned: ', sensorData);
+    });
+    });
+    });
+
+    // Connected status to console
+    socket.on('my response', function(msg) {
+        console.log('socket.io server connected');
+    });
+
+    // Timelapse update info
+    socket.on('timelapseUpdate', function(timeData) {
+        console.log('timeData returned: ', timeData);
+
+        // Disable fields and submit button until finished
+        if (timeData.loadStatus === "True") {
+            $('#daysInput').attr('disabled', true);
+            $('#hrsInput').attr('disabled', true);
+            $('#minsInput').attr('disabled', true);
+            $('#secsInput').attr('disabled', true);
+            $('#delayInput').attr('disabled', true);
+            $('#timelapseBtn').attr('disabled', true);
+        }
+        else {
+            $('#daysInput').attr('disabled', false);
+            $('#hrsInput').attr('disabled', false);
+            $('#minsInput').attr('disabled', false);
+            $('#secsInput').attr('disabled', false);
+            $('#delayInput').attr('disabled', false);
+            $('#timelapseBtn').attr('disabled', false);
+        }
+        
+        // create total frame section
+        if (timeData.loadStatus === "True") {
+            $('#totalFrameSec').empty();
+            let frameCount = $('<p>');
+            $(frameCount).addClass('frameText');
+            $(frameCount).text("Total Frames: " + timeData.totalFrames);
+            $('#totalFrameSec').append(frameCount);
+        }
+        else {
+            $('#totalFrameSec').empty();
+        }
+
+        
+        // create remaining frame section
+        if (timeData.loadStatus === "True") {
+            $('#remainingFrameSec').empty();
+            let remaingFrames = $('<p>');
+            $(remaingFrames).addClass('frameText');
+            $(remaingFrames).text("Remaining Frames: " + timeData.framesRemaining);
+            $('#remainingFrameSec').append(remaingFrames);
+        }
+        else {
+            $('#remainingFrameSec').empty();
+        }
+
+        // create loading image section
+        if (timeData.loadStatus === "True") {
+            $('#timeImgSec').empty();
+            let loadingImg = $('<img>');
+            $(loadingImg).attr("id","timelapseLoadingGif");
+            loadingImg.attr('src', './static/imgs/loadingcat.gif');
+            loadingImg.attr('alt', 'loading toaster cat');
+            $('#timeImgSec').append(loadingImg);
+        }
+        else {
+            $('#timeImgSec').empty();
+        }
+        
+        
+        // $('#timelapseData').append(frameCount, remaingFrames, loadingImg);
+    });
+
+    $(function() { $("#timelapseBtn").click(function (e) { 
+        e.preventDefault();
+        // gather values
+        let daysVal = $('#daysInput').val();
+        let hrsVal = $('#hrsInput').val();
+        let minsVal = $('#minsInput').val();
+        let secsVal = $('#secsInput').val();
+        let delayVal = $('#delayInput').val();
+
+        // no data string to integer catch
+        if (!daysVal || daysVal == "") {
+            daysVal = 0;
+        }
+
+        if (!hrsVal || hrsVal == "") {
+            hrsVal = 0;
+        }
+
+        if (!minsVal || minsVal == "") {
+            minsVal = 0;
+        }
+
+        if (!secsVal || secsVal == "") {
+            secsVal = 0;
+        }
+
+        if (!delayVal || delayVal == "") {
+            delayVal = 0;
+        }
+
+        // checks for no data or lack of enough timelapse data
+        let sumInput = daysVal + hrsVal + minsVal + secsVal
+        let divCheck = sumInput / delayVal
+        console.log("sumInput", sumInput)
+        console.log("divCheck", divCheck)
+        console.log('infinite check: ', isFinite(divCheck), !isFinite(divCheck) )
+        if((sumInput > 0) && (divCheck > 1) && (isFinite(divCheck))) {
+
+            console.log('Days: ', daysVal, 'hrs: ', hrsVal, 'mins: ', minsVal, 'secs: ', secsVal, 'delayInput: ', delayVal);
+
+            socket.emit('my event', {
+                daysValue: daysVal,
+                hrsValue: hrsVal,
+                minsValue: minsVal,
+                secsValue: secsVal,
+                delayValue: delayVal  
+            });
+
+            // trigger snap button animation
+            // select and grab snap photo button by id tag
+            const element = document.querySelector('#timelapseBtn');
+            // add animation class to snap button via stored id element
+            element.classList.add('animate__animated', 'animate__rubberBand');
+            // Add detection to remove animation class
+            element.addEventListener('animationend', () => {
+                element.classList.remove('animate__animated', 'animate__rubberBand');
+            });
+
+            return false;
+        }
+        else {
+            console.log('deciding popover');
+            // conditional to trigger needed popover feedback 
+            if(sumInput == 0 && !isFinite(divCheck)){
+                console.log('timelapse button popover')
+                $('#timelapseBtn').popover("toggle");
+                setTimeout(function(){ $('#timelapseBtn').popover("toggle"); }, 4000);
+            } 
+            else if((sumInput != 0) && (divCheck < 1) && (isFinite(divCheck))) {
+                console.log('delay input field popover')
+                $('#delayDiv').popover("toggle");
+                setTimeout(function(){ $('#delayDiv').popover("toggle"); }, 4000);
+            }
+            // else if(isFinite(divCheck)) {
+            //     console.log('infinity check')
+            // }
+        }
+        });
+    });
+    // $('form#broadcast').submit(function(event) {
+    //     socket.emit('my broadcast event', {data: $('#broadcast_data').val()});
+    //     return false;
+    // });
+
+    // --------------------- Flask WebSocketIO END -------------------------------
+
+    // Add listener to gallery images to launch modal
+    $(document).on('click', ".galleryImg", function(e){ 
+        e.preventDefault();
+        let filename = $(this).attr("src");
+        $(loadingGif).attr("src", filename);
+        $('#photoModal').modal('show');
+    });    
+
+    // Event listener to delete photo
+    $(function() { $("#deleteBtn").click(function (e) { 
+        e.preventDefault();
+        let filename = $('#loadingGif').attr('src');
+            filename = filename.slice(2);
+            filename = '/home/pi/shared/3DPrinterCam/' + filename;
+        $.post('/PhotoDelete', {filenameVal: filename}, function(result) {
+            grabGalleryData();
+            });
+        });    
+    });
+
+    // Add event listener to id snapBtn and trigger a snapshot
+    $(function() { $("#snapBtn").click(function (e) { 
+        e.preventDefault();
+        console.log('event triggered');
+        $.post('/PhotoSnap', {}, function(result) {
+            grabGalleryData();
+            // trigger snap button animation
+            // select and grab snap photo button by id tag
+            const element = document.querySelector('#snapBtn');
+            // add animation class to snap button via stored id element
+            element.classList.add('animate__animated', 'animate__rubberBand');
+            // Add detection to remove animation class
+            element.addEventListener('animationend', () => {
+            element.classList.remove('animate__animated', 'animate__rubberBand');
+            });
+        });    
+    });
+    });
+
+    // Grab photo data
+    grabGalleryData = () => {
+        $.getJSON('/photoGalleryBuild',
+        function(data) {
+            if (data || data.imgArray) {
+                storePhotoData(data.imgArray);
+            }
+            else {
+                console.log('NoData');
+            }
+    })}
+
+    // Store array locally
+    storePhotoData = (photoArr) => {
+        localStorage.setItem("photoArr", JSON.stringify(photoArr));
+        getNumOfPages();
+    }
+
+    // Get variables and calculate range
+    getNumOfPages = () => {
+        lenOfArr = (JSON.parse(localStorage.getItem("photoArr"))).length;
+        numOfPages = Math.ceil(lenOfArr / numPerPage);
+        loadPageList();
+    }
+
+    // Slice array
+    loadPageList = () => {
+        let begin = ((currentPage - 1) * numPerPage);
+        let end = begin + numPerPage;
+        let bulkArr = JSON.parse(localStorage.getItem("photoArr"));
+        pageListArr = bulkArr.slice(begin, end);
+        populateGallery();
+        check();
+    }
+
+    // Gallery creation
+    populateGallery = () => {
+
+        // Establish Current Page
+        $('#currentPG').val(currentPage)
+
+        // Clear and build image gallery
+        $('#photoGallery').empty();
+        for(i=0; i < pageListArr.length; i++) {
+            let startDiv = $('<div>');
+            $(startDiv).addClass('col-3');
+            let startImg = $('<img>');
+            $(startImg).addClass('galleryImg img-thumbnail img-fluid mb-3');
+            $(startImg).attr('src', '../static/imgs/captures/' + pageListArr[i]);
+            $(startImg).attr('alt', 'printer snapshot');
+            $(startDiv).append(startImg);
+            $('#photoGallery').append(startDiv);
+        }
+
+        // Populate pages index for nav 
+    }
+
+    // Check # of pages to disble button status
+    check = () => {
+        if(currentPage == numOfPages){
+            $("#next").prop("disabled", true);
+        }
+        else {
+            $("#next").prop("disabled", false);
+        }
+
+        if(currentPage == 1){
+            $("#previous").prop("disabled", true);
+            $("#first").prop("disabled", true)
+        }
+        else {
+            $("#previous").prop("disabled", false);
+            $("#first").prop("disabled", false);
+        }
+
+        if(currentPage == numOfPages){
+            $("#last").prop("disabled", true);
+        }
+        else {
+            $("#last").prop("disabled", false);
+        }
+    }
+
+    // Gallery navigation buttons
+    nextPage = () => {
+        currentPage += 1;
+        loadPageList();
+    }
+    
+    previousPage = () => {
+        currentPage -= 1;
+        loadPageList();
+    }
+    
+    firstPage = () => {
+        currentPage = 1;
+        loadPageList();
+    }
+    
+    lastPage = () => {
+        currentPage = numOfPages;
+        loadPageList();
+    }
+
+    // Run at page load
+    grabGalleryData();
+
+});
